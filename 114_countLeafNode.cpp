fc4e21bfd46f555b874f4380e27bf565eb711fb9
Abhinam <103098871+khatiyan@users.noreply.github.com>
2023-04-30 23:27:49 +0530

    Add files via upload

---
+++ 114_countLeafNode.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+template <typename T>
+class BinaryTreeNode {
+      public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+                this -> data = data;
+                left = NULL;
+                right = NULL;
+        }
+    };
+void inorder(BinaryTreeNode<int> *root,int &count){
+    //base case
+    if (root == NULL) {
+      return;
+    }
+
+    //LNR
+    inorder(root->left,count);
+
+    if (root->left == NULL && root->right==NULL) {
+      count++;
+    }
+
+    inorder(root->right,count);
+}
+
+int noOfLeafNodes(BinaryTreeNode<int> *root){
+    // Write your code here.
+
+    int count=0;
+    inorder(root,count);
+    return count;
+
+}
